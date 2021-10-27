6ccf65265727e7e8e4002c473af4b3ac74d17a24
Philip Czymmek <philip@czymmek.me>
2021-10-27 16:58:13 +0200

    ch03 started

--- ch03/src/main.rs
+++ ch03/src/main.rs
@@ -3,4 +3,17 @@ fn main() {
     println!("{}", x);
     x = 6;
     println!("{}", x);
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("{}", spaces);
+
+    let overflow: u8 = 200;
+    let overflow = overflow.wrapping_add(57);
+    let overflow = overflow.overflowing_add(u8::MAX);
+
+    println!("{:?}", overflow);
 }
